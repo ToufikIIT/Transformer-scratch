75a3cf5078f8bb822fcb7d7f1f6d6b4afec7680a
Toufik <toufik.haque2021@gmail.com>
2026-05-06 16:17:45 +0530

    implemented multihead attention

--- model.py
+++ model.py
@@ -59,4 +59,54 @@ class FeedForwardBlock(nn.Module):
     def forward(self,x):
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
         
+class MultiAttentionBlock(nn.Module):
+    
+    def __init__(self, d_model:int, h:int, dropout:float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        self.dropout = nn.Dropout(dropout)
         
+        assert d_model % h == 0, "d_model must be divisible by h"
+        
+        self.d_k = d_model // h
+        
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+        
+        self.w_o = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+        
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+        
+        # (Batch, h, seq_len, d_k) @ (Batch, h, d_k, seq_len) -> (Batch, h, seq_len, seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            attention_scores = attention_scores.masked_fill(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim=-1)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+        
+        output = attention_scores @ value
+        return output, attention_scores
+        
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) 
+        key = self.w_k(k)
+        value = self.w_v(v)
+        
+        #(Batch, seq_len , d_model) -> (Batch, seq_len, h, d_k) -> (Batch, h, seq_len, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
+        
+        x, self.attention_scores = MultiAttentionBlock.attention(query, key, value, mask, self.dropout)
+        
+        #(Batch, h, seq_len, d_k) -> (Batch, seq_len, h, d_k) -> (Batch, seq_len, d_model)
+        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+        
+        # (Batch, seq_len, d_model) -> (Batch, seq_len, d_model)
+        return self.w_o(x)
