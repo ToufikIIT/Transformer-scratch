05c3d48880de2ffa20bb91c2454f3db7c8ad7070
Toufik <toufik.haque2021@gmail.com>
2026-05-07 18:52:48 +0530

    dataset

---
+++ dataset.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset,DataLoader, random_split
+
+class BilingualDataset(Dataset):
+    def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len) -> None:
+        super().__init__()
+        
+        self.ds_raw = ds
+        self.tokenizer_src = tokenizer_src
+        self.tokenizer_tgt = tokenizer_tgt
+        self.src_lang = src_lang
+        self.tgt_lang = tgt_lang
+        self.seq_len = seq_len
+        
+        
